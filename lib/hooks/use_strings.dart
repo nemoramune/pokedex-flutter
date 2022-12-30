bab656200b0dd0aeb5da0b106a257a1142f96df0
nemoramune <nemoramune@gmail.com>
2022-12-31 02:12:39 +0900

    Add slang string resource package to dependencies

---
+++ lib/hooks/use_strings.dart
@@ -0,0 +1,3 @@
+import 'package:pokedex/i18n/strings.g.dart';
+
+StringsJa useStrings() => t;
--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'features/pokemon/components/pokemon_list_page.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized(); // add this
+  // LocaleSettings.useDeviceLocale(); // and this
   runApp(const ProviderScope(child: MyApp()));
 }
 
