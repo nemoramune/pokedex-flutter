43032500ffca0274b841b9fb5f445a1c712c5d81
nemoramune <nemoramune@gmail.com>
2023-01-05 13:05:07 +0900

    Change PokemonListItemView for routing

--- lib/features/pokemon/components/pokemon_list_item_view.dart
+++ lib/features/pokemon/components/pokemon_list_item_view.dart
@@ -2,6 +2,7 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:pokedex/features/pokemon/model/pokemon_list_item.dart';
+import 'package:pokedex/routes/routes.dart';
 
 class PokemonListItemView extends StatelessWidget {
   const PokemonListItemView({
@@ -15,44 +16,50 @@ class PokemonListItemView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        constraints: const BoxConstraints(maxWidth: 640),
-        child: IntrinsicHeight(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              const Gap(4),
-              CachedNetworkImage(
-                imageUrl: data.imageUrl,
-                width: 128,
-                height: 128,
-                fit: BoxFit.fitHeight,
-                placeholder: (context, url) => const Center(child: CircularProgressIndicator()),
-                errorWidget: (context, url, error) =>
-                    const Center(child: Icon(Icons.error_outline, size: 64, color: Colors.red)),
-              ),
-              Container(
-                constraints: const BoxConstraints(maxWidth: 220),
-                child: _PokemonListItemInfo(data: data),
-              ),
-              Center(
-                child: IconButton(
-                  iconSize: 32,
-                  color: data.isFavorite ? Colors.red : Colors.grey,
-                  isSelected: data.isFavorite,
-                  style: ButtonStyle(
-                    shape: MaterialStateProperty.all(const CircleBorder()),
-                    padding: MaterialStateProperty.all(const EdgeInsets.all(16)),
-                  ),
-                  icon: const Icon(Icons.favorite_border),
-                  selectedIcon: const Icon(Icons.favorite),
-                  onPressed: () => onPressedFavorite(data),
+    return GestureDetector(
+      behavior: HitTestBehavior.opaque,
+      onTap: () {
+        PokemonDetailRoute(id: data.id).go(context);
+      },
+      child: Center(
+        child: Container(
+          constraints: const BoxConstraints(maxWidth: 640),
+          child: IntrinsicHeight(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const Gap(4),
+                CachedNetworkImage(
+                  imageUrl: data.imageUrl,
+                  width: 128,
+                  height: 128,
+                  fit: BoxFit.fitHeight,
+                  placeholder: (context, url) => const Center(child: CircularProgressIndicator()),
+                  errorWidget: (context, url, error) =>
+                      const Center(child: Icon(Icons.error_outline, size: 64, color: Colors.red)),
                 ),
-              ),
-              const Gap(4),
-            ],
+                Container(
+                  constraints: const BoxConstraints(maxWidth: 220),
+                  child: _PokemonListItemInfo(data: data),
+                ),
+                Center(
+                  child: IconButton(
+                    iconSize: 32,
+                    color: data.isFavorite ? Colors.red : Colors.grey,
+                    isSelected: data.isFavorite,
+                    style: ButtonStyle(
+                      shape: MaterialStateProperty.all(const CircleBorder()),
+                      padding: MaterialStateProperty.all(const EdgeInsets.all(16)),
+                    ),
+                    icon: const Icon(Icons.favorite_border),
+                    selectedIcon: const Icon(Icons.favorite),
+                    onPressed: () => onPressedFavorite(data),
+                  ),
+                ),
+                const Gap(4),
+              ],
+            ),
           ),
         ),
       ),
