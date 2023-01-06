83a81b54298aae9b9a15393a61c7ab1c8dd4e676
nemoramune <nemoramune@gmail.com>
2023-01-06 13:54:03 +0900

    Add PokemonDetailState

---
+++ lib/features/pokemon/states/pokemon_detail_state.dart
@@ -0,0 +1,11 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:pokedex/features/pokemon/model/pokemon_detail.dart';
+
+part 'pokemon_detail_state.freezed.dart';
+
+@freezed
+class PokemonDetailState with _$PokemonDetailState {
+  const factory PokemonDetailState({
+    PokemonDetail? data,
+  }) = _PokemonDetailState;
+}
