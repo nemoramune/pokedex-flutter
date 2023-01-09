792c33fdd95f78b48f1893de0e04f3f6bbc2cf8c
nemoramune <nemoramune@gmail.com>
2023-01-09 18:02:17 +0900

    Add favorites routes

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,17 +1,14 @@
 import 'package:flutter/widgets.dart';
 import 'package:go_router/go_router.dart';
 import 'package:pokedex/features/pokemon/components/pokemon_detail_page.dart';
+import 'package:pokedex/features/pokemon/components/pokemon_favorites_page.dart';
 import 'package:pokedex/features/pokemon/components/pokemon_list_page.dart';
 
 part 'routes.g.dart';
 
 @TypedGoRoute<PokemonListRoute>(
   path: '/',
-  routes: <TypedGoRoute<GoRouteData>>[
-    TypedGoRoute<PokemonDetailRoute>(
-      path: ':id',
-    )
-  ],
+  routes: [TypedGoRoute<PokemonListDetailRoute>(path: 'pokemon/:id')],
 )
 class PokemonListRoute extends GoRouteData {
   @override
@@ -20,8 +17,28 @@ class PokemonListRoute extends GoRouteData {
   }
 }
 
-class PokemonDetailRoute extends GoRouteData {
-  PokemonDetailRoute({required this.id});
+class PokemonListDetailRoute extends GoRouteData {
+  PokemonListDetailRoute({required this.id});
+  final int id;
+  @override
+  Widget build(BuildContext context, GoRouterState state) {
+    return PokemonDetailPage(id: id);
+  }
+}
+
+@TypedGoRoute<PokemonFavoritesRoute>(
+  path: '/favorites',
+  routes: [TypedGoRoute<PokemonFavoritesDetailRoute>(path: ':id')],
+)
+class PokemonFavoritesRoute extends GoRouteData {
+  @override
+  Widget build(BuildContext context, GoRouterState state) {
+    return const PokemonFavoritesPage();
+  }
+}
+
+class PokemonFavoritesDetailRoute extends GoRouteData {
+  PokemonFavoritesDetailRoute({required this.id});
   final int id;
   @override
   Widget build(BuildContext context, GoRouterState state) {
