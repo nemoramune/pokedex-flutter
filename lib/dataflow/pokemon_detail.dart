81f7c553996b32f49fe5daf7dd71eb1e4a1e6b9c
nemoramune <nemoramune@gmail.com>
2023-02-12 14:58:51 +0900

    Change to refactor pokemonDetailProvider

--- lib/dataflow/pokemon_detail.dart
+++ lib/dataflow/pokemon_detail.dart
@@ -6,7 +6,5 @@ import 'pokedex.dart';
 part 'pokemon_detail.g.dart';
 
 @riverpod
-Future<Pokemon> pokemon(PokemonRef ref, int id) async {
-  await ref.read(fetchPokemonDataProvider([id]).future);
-  return ref.watch(pokeDexProvider.selectAsync((data) => data[id]!));
-}
+Future<Pokemon> pokemon(PokemonRef ref, int id) =>
+    ref.watch(getPokemonDataListProvider([id]).selectAsync((data) => data.first));
