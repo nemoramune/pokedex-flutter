16fad2d0b7df9b4dc705952f558059f5be2dff89
nemoramune <nemoramune@gmail.com>
2023-02-12 13:39:22 +0900

    Change to refactor pokemonListProvider

--- lib/dataflow/pokemon_list.dart
+++ lib/dataflow/pokemon_list.dart
@@ -9,7 +9,7 @@ part 'pokemon_list.g.dart';
 const _limit = 20;
 
 // StateProviderは外部からstateを更新できるがNotifierは外部からstateを更新することはできない
-final _pokemonListSizeProvider = StateProvider<int>((_) => _limit);
+final _pokemonListSizeProvider = StateProvider((_) => _limit);
 
 final isPokemonListLastProvider = StateProvider.autoDispose<bool>((_) => false);
 
@@ -18,19 +18,10 @@ Function loadListNextPage(LoadListNextPageRef ref) =>
     () => ref.read(_pokemonListSizeProvider.notifier).update((state) => state + _limit);
 
 @riverpod
-Future<List<int>> fetchedPokemonListIds(FetchedPokemonListIdsRef ref) async {
+Future<List<Pokemon>> pokemonList(PokemonListRef ref) async {
   final pokemonListSize = ref.watch(_pokemonListSizeProvider);
   final ids = List.generate(pokemonListSize, (index) => index + 1);
-  await ref.read(fetchPokemonDataProvider(ids).future);
-  return ids;
-}
-
-@riverpod
-Future<List<Pokemon>> pokemonList(PokemonListRef ref) async {
-  final pokemonListSize = ref.read(_pokemonListSizeProvider);
-  final ids = await ref.watch(fetchedPokemonListIdsProvider.future);
-  final list = await ref.watch(pokeDexProvider
-      .selectAsync((data) => ids.map((id) => data[id]).whereType<Pokemon>().toList()));
+  final list = await ref.watch(getPokemonDataListProvider(ids).future);
   ref.read(isPokemonListLastProvider.notifier).update((state) => list.length < pokemonListSize);
   return list;
 }
